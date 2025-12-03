df9573ea4542819ed39e7d0a90fbe3ed1ea4a8df
tara944 <koranga134@gmail.com>
2025-12-03 15:23:50 +0530

    deleted modules

--- indiexpert student module/view_all_module.py
+++
@@ -1,8 +0,0 @@
-from ragister_module import list_deta
-def view_all():
-    if len(list_deta) == 0:
-        print("No students registered.")
-    else:
-        print("\n=========== ALL STUDENTS ===========")
-        for s in list_deta:
-            print(s)
